b30faa1c64c9a6791f24c4b55eee76453af6ffda
Tom Seldon <tom@tomseldon.co.uk>
2016-11-10 16:37:00 +0000

    [BUG] Fix issue with Travis commit sha
    When using "TRAVIS_COMMIT" we were expecting this to be the SHA of the latest commit on the pull request.
    
    It turns out that Travis first merges the base branch into the branch under test, which means the latest commit (the one references by TRAVIS_COMMIT) is a merge commit, and not the one that we want to test.
    
    To work around this, this commit instead uses "TRAVIS_PULL_REQUEST_SHA" to get the commit SHA.

--- tools/travis-commit-message-checker.js
+++ tools/travis-commit-message-checker.js
@@ -5,10 +5,20 @@
 const lib = require('../lib');
 const reporter = require('../reporter').resultFormatter;
 
-const commitSHA = process.env['TRAVIS_COMMIT'];
-
 // TODO: For PR build we should check all commits in the pull-request, not just the latest one.
 //       This is more problematic than it sounds (though not impossible), and should be addressed seperately.
+
+// Currently we only test PR builds, where we get the HEAD commit.
+
+const isPullRequest = process.env['TRAVIS_PULL_REQUEST'] === 'true';
+
+if (!isPullRequest) {
+    console.log('Not checking commit message as this is not a pull-request build');
+    return;
+}
+
+// Note that we can't use "TRAVIS_COMMIT" here as we'll get a merge commit
+const commitSHA = process.env['TRAVIS_PULL_REQUEST_SHA'];
 
 lib.getCommitMessageFromSHA(commitSHA)
     .catch(error => {
