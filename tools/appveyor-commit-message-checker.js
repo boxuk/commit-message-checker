cde593e760bfc64b6d7bc64073bf2815234b7d50
Tom Seldon <tom@tomseldon.co.uk>
2016-11-16 18:04:31 +0000

    [FEATURE] Appveyor: Handle PR commit range
    Modify Appveyor tool to test the range of commits in the PR.
    
    Also output the number of commit messages checked and the number that failed.

--- tools/appveyor-commit-message-checker.js
+++ tools/appveyor-commit-message-checker.js
@@ -5,11 +5,6 @@
 const lib = require('../lib');
 const reporter = require('../reporter').resultFormatter;
 
-// TODO: For PR build we should check all commits in the pull-request, not just the latest one.
-//       This is more problematic than it sounds (though not impossible), and should be addressed seperately.
-
-// Currently we only test PR builds, where we get the HEAD commit.
-
 const isPullRequest = Boolean(process.env.APPVEYOR_PULL_REQUEST_NUMBER);
 
 if (!isPullRequest) {
@@ -18,21 +13,33 @@ if (!isPullRequest) {
     return;
 }
 
-const commitSHA = process.env.APPVEYOR_REPO_COMMIT;
+const baseBranch = process.env.APPVEYOR_REPO_BRANCH;
 
-lib.getCommitMessageFromSHA(commitSHA)
+// For the commit range, we need to get all commits since the base branch, up to the current HEAD.
+const commitRange = `${baseBranch}..HEAD`;
+
+lib.getCommitMessagesFromSHARange(commitRange)
     .catch(error => {
-        // If we failed to get the commit message then fail the build
-        console.error(`Failed to retrieve commit message: ${error}`);
+        // If we failed to get the commit messages then fail the build
+        console.error(`Failed to retrieve commit messages: ${error}`);
 
         process.exit(1);
     })
-    .then(commitMessage => {
-        const validation = lib.validateCommitMessage(commitMessage);
+    .then(commitMessages => {
+        const results = commitMessages.map(commitMessage => ({
+            validation: lib.validateCommitMessage(commitMessage),
+            commitMessage: commitMessage
+        }));
 
-        // If the commit message is invalid, output some helpful information and then exit with non-zero code
-        if (!validation.isValid) {
-            console.error(reporter(commitMessage, validation));
+        const failures = results.filter(result => result.validation.isValid === false);
+
+        console.log(`Tested ${commitMessages.length} commit messages, ${failures.length} were invalid`);
+
+        // If any of the commit message are invalid, output some helpful information and then exit with non-zero code
+        if (failures.length) {
+            failures.map(failure => console.error(reporter(failure.commitMessage, failure.validation)));
+
+            console.error(`${failures.length} commit messages are in an invalid format`);
 
             process.exit(1);
         }
