0d4d577f797a76b63421afc68b904a16ac817315
Tom Seldon <tom@tomseldon.co.uk>
2016-11-17 13:10:50 +0000

    [FEATURE] Appveyor: Add support for push builds
    Modify Appveyor tool to support branch/push builds, in addition to pull-request builds.

--- tools/appveyor-commit-message-checker.js
+++ tools/appveyor-commit-message-checker.js
@@ -7,40 +7,58 @@ const reporter = require('../reporter').resultFormatter;
 
 const isPullRequest = Boolean(process.env.APPVEYOR_PULL_REQUEST_NUMBER);
 
-if (!isPullRequest) {
-    console.log('Not checking commit message as this is not a pull-request build');
+if (isPullRequest) {
+    const baseBranch = process.env.APPVEYOR_REPO_BRANCH;
 
-    return;
-}
+    // For the commit range, we need to get all commits since the base branch, up to the current HEAD.
+    const commitRange = `${baseBranch}..HEAD`;
 
-const baseBranch = process.env.APPVEYOR_REPO_BRANCH;
-
-// For the commit range, we need to get all commits since the base branch, up to the current HEAD.
-const commitRange = `${baseBranch}..HEAD`;
-
-lib.getCommitMessagesFromSHARange(commitRange)
-    .catch(error => {
-        // If we failed to get the commit messages then fail the build
-        console.error(`Failed to retrieve commit messages: ${error}`);
-
-        process.exit(1);
-    })
-    .then(commitMessages => {
-        const results = commitMessages.map(commitMessage => ({
-            validation: lib.validateCommitMessage(commitMessage),
-            commitMessage: commitMessage
-        }));
-
-        const failures = results.filter(result => result.validation.isValid === false);
-
-        console.log(`Tested ${commitMessages.length} commit messages, ${failures.length} were invalid`);
-
-        // If any of the commit message are invalid, output some helpful information and then exit with non-zero code
-        if (failures.length) {
-            failures.map(failure => console.error(reporter(failure.commitMessage, failure.validation)));
-
-            console.error(`${failures.length} commit messages are in an invalid format`);
+    lib.getCommitMessagesFromSHARange(commitRange)
+        .catch(error => {
+            // If we failed to get the commit messages then fail the build
+            console.error(`Failed to retrieve commit messages: ${error}`);
 
             process.exit(1);
-        }
-    });
+        })
+        .then(commitMessages => {
+            const results = commitMessages.map(commitMessage => ({
+                validation: lib.validateCommitMessage(commitMessage),
+                commitMessage: commitMessage
+            }));
+
+            const failures = results.filter(result => result.validation.isValid === false);
+
+            console.log(`Tested ${commitMessages.length} commit messages, ${failures.length} were invalid`);
+
+            // If any of the commit message are invalid, output some helpful information and then exit with non-zero code
+            if (failures.length) {
+                failures.map(failure => console.error(reporter(failure.commitMessage, failure.validation)));
+
+                console.error(`${failures.length} commit messages are in an invalid format`);
+
+                process.exit(1);
+            }
+        });
+} else {
+    // This is a branch build, so just check the latest commit that has been pushed to the branch
+
+    const commitSHA = process.env.APPVEYOR_REPO_COMMIT;
+
+    lib.getCommitMessageFromSHA(commitSHA)
+        .catch(error => {
+            // If we failed to get the commit message then fail the build
+            console.error(`Failed to retrieve commit message: ${error}`);
+
+            process.exit(1);
+        })
+        .then(commitMessage => {
+            const validation = lib.validateCommitMessage(commitMessage);
+
+            if (!validation.isValid) {
+                // The commit message was invalid, so output some helpful information and then exit with non-zero code
+                console.error(reporter(commitMessage, validation));
+
+                process.exit(1);
+            }
+        });
+}
