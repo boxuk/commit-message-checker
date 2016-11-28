2d25632b4ef96fc21f13e0af85467fa36a6d09cb
Tom Seldon <tom@tomseldon.co.uk>
2016-11-28 09:54:38 +0000

    [FEATURE] Add tests for invalid commit messages

---
+++ test/end-to-end/invalid-commit.spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const chai = require('chai');
+const sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
+
+const expect = chai.expect;
+
+const library = require('../../lib');
+const failureReasons = require('../../lib/failure-reasons').FAILURE_REASONS;
+
+describe('checking invalid commit messages', () => {
+    it('should treat "Test commit" as an invalid commit message, due to lack of commit type', () => {
+        expect(library.isValidCommitMessage('Test commit')).to.be.false;
+
+        expect(library.validateCommitMessage('Test commit').failures)
+            .to.contain(failureReasons.missingOrInvalidCommitType);
+    });
+
+    it('should treat a multi-line message as invalid if there is no new line between summary and description', () => {
+        const commitMessage = '[BUG] Some bug fix\nFix issue with some feature';
+
+        expect(library.isValidCommitMessage(commitMessage)).to.be.false;
+
+        expect(library.validateCommitMessage(commitMessage).failures)
+            .to.contain(failureReasons.noNewLineAfterFirstLine);
+    });
+});
